e31a0b0a55172405b6c6b8c7ce9f2a6d7ff988ec
Астахов Артём <artemastahov27@gmail.com>
2024-09-24 23:37:08 +0300

    Поменял реализацию лабы по ОС

--- LabsOS/SecondLab/main.cpp
+++ LabsOS/SecondLab/main.cpp
@@ -1,77 +1,44 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <vector>
 
-void printProcessInfo(const std::string &message) {
+void printProcessInfo(const std::string& message) {
     pid_t pid = getpid();
     pid_t ppid = getppid();
     std::cout << message << " (PID: " << pid << ", PID родителя: " << ppid << ")" << std::endl;
 }
 
-void waitForAllChildren() {
-    while (wait(NULL) > 0);
-}
-
 int main() {
+    std::vector<int> tree = {0, 0, 1, 1, 2, 3, 4, 5};
+
+    pid_t pids[tree.size()];
     printProcessInfo("Основной процесс запущен");
 
-    pid_t pid_1 = fork();
+    for (size_t i = 0; i < tree.size(); ++i) {
+        if (i == 0 || pids[tree[i]] == getpid()) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                printProcessInfo("Дочерний процесс " + std::to_string(i) + " запущен");
 
-    if (pid_1 == 0) {
-        printProcessInfo("Дочерний процесс 1 запущен");
-
-        pid_t pid_2 = fork();
-        if (pid_2 == 0) {
-            printProcessInfo("Дочерний процесс 2 запущен");
-
-            pid_t pid_3 = fork();
-            if (pid_3 == 0) {
-                printProcessInfo("Дочерний процесс 3 запущен");
-
-                pid_t pid_5 = fork();
-                if (pid_5 == 0) {
-                    printProcessInfo("Дочерний процесс 5 запущен");
-
-                    pid_t pid_7 = fork();
-                    if (pid_7 == 0) {
-                        printProcessInfo("Дочерний процесс 7 запущен");
-                        _exit(0);
-                    }
-
-                    waitForAllChildren();
-                    _exit(0);
+                if (i == 1) {
+                    std::cout << "Процесс " << i << " будет выполнять команду pwd позже." << std::endl;
                 }
 
-                waitForAllChildren();
-                _exit(0);
+                pids[i] = getpid();
+            } else if (pid > 0) {
+                pids[i] = pid;
+                wait(NULL);
             }
-
-            pid_t pid_4 = fork();
-            if (pid_4 == 0) {
-                printProcessInfo("Дочерний процесс 4 запущен");
-
-                pid_t pid_6 = fork();
-                if (pid_6 == 0) {
-                    printProcessInfo("Дочерний процесс 6 запущен");
-                    _exit(0);
-                }
-
-                waitForAllChildren();
-                _exit(0);
-            }
-
-            waitForAllChildren();
-            _exit(0);
         }
-
-        execlp("pwd", "pwd", NULL);
-
-        waitForAllChildren();
-        printProcessInfo("Дочерний процесс 1 завершает работу");
-        _exit(0);
     }
 
-    waitForAllChildren();
+    if (pids[1] == getpid()) {
+        std::cout << "Процесс 1 выполняет команду pwd:" << std::endl;
+        execl("/bin/pwd", "pwd", (char *)0);
+        _exit(0); 
+    }
+
     printProcessInfo("Основной процесс завершает работу");
     return 0;
 }
